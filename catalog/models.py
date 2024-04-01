48d9de326004fc0b0abc607a5e255c7ae1e5d6cc
Krimsi13 <aleksejkrimsi@gmail.com>
2024-04-01 21:35:33 +0300

    hw_20_1_exerc_2

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=250, verbose_name='Наименование')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=250, verbose_name='Наименование')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
