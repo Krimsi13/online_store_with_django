2a9c025c9166bffd36d3a02abc53fa0a9e80fc34
Krimsi13 <aleksejkrimsi@gmail.com>
2024-04-01 22:54:33 +0300

    hw_20_1_exerc_3

--- catalog/models.py
+++ catalog/models.py
@@ -1,19 +1,11 @@
 from django.db import models
 
-
-class Product(models.Model):
-    name = models.CharField(max_length=250, verbose_name='Наименование')
-
-    def __str__(self):
-        return f'{self.name}'
-
-    class Meta:
-        verbose_name = 'продукт'
-        verbose_name_plural = 'продукты'
+NULLABLE = {'null': True, 'blank': True}
 
 
 class Category(models.Model):
     name = models.CharField(max_length=250, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
 
     def __str__(self):
         return f'{self.name}'
@@ -21,3 +13,20 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    preview = models.ImageField(upload_to='product/', verbose_name='Изображение', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price_per_purchase = models.IntegerField(default=0)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
