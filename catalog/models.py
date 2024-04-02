8ffb130406f5dc4b2ec965d1eb5f7b39158cf9f7
Krimsi13 <aleksejkrimsi@gmail.com>
2024-04-02 20:46:58 +0300

    hw_20_1_exerc_4

--- catalog/models.py
+++ catalog/models.py
@@ -19,10 +19,11 @@ class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
     preview = models.ImageField(upload_to='product/', verbose_name='Изображение', **NULLABLE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    price_per_purchase = models.IntegerField(default=0)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    price_per_purchase = models.IntegerField(default=0, verbose_name='Цена за покупку')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+    # manufactured_at = models.DateTimeField(**NULLABLE, verbose_name='Дата производства продукта')
 
     def __str__(self):
         return f'{self.name}'
