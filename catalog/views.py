fb33ea3924e7eccc9bc6869c5e274e345f6fa94d
Krimsi13 <aleksejkrimsi@gmail.com>
2024-04-10 22:15:46 +0300

    hw_20_2_exerc_3

--- catalog/views.py
+++ catalog/views.py
@@ -4,17 +4,19 @@ from catalog.models import Product
 
 
 def index(request):
-    context = {'object_list': Product.objects.all()}
+    context = {'object_list': Product.objects.all(),
+               'title': 'Главная'}
     return render(request, 'catalog/index.html', context)
 
 
 def contacts(request):
+    context = {'title': 'Контакты'}
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f'{name} ({phone}): {message}')
-    return render(request, 'catalog/contacts.html')
+    return render(request, 'catalog/contacts.html', context)
 
 
 def product(request, pk):
