5f75c7ac6160e63a56f9e82cd7a823a8d1204a34
Krimsi13 <aleksejkrimsi@gmail.com>
2024-04-20 21:34:28 +0300

    hw_21_1_fix_1_create_blogadmin

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from blogs.models import Blog
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "is_published", "views_count",)
